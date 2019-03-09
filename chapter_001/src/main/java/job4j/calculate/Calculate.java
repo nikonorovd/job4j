735278d608acbb7227f178b60eb572b6c4618772
Dmitriy Nikonorov <nikonorovds@yandex.ru>
2019-03-09 12:19:44 +0300

    1.3. JavaDoc. Code convention[#101200]

--- chapter_001/src/main/java/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/job4j/calculate/Calculate.java
@@ -1,12 +1,28 @@
 package ru.nikonorov;
-
+/** Класс Calculate для вычисления арифметических операций + - / * .
+* @author Nikonorov.
+* @since 08.03.2019.
+* @version 1.
+*/ 
 public class Calculate {
+	/** метод echo.
+	* @param value.
+	* @return Echo 3 раза.
+	*/
     public String echo(String value) {
+		
         return String.format("%s %s %s", value, value, value);
     }
 
+
+/** @param args - args
+* calc - новый объект метода main.
+* @return сообщение "aah".
+*/
+
     public static void main(String[] args) {
-        Calculate calc = new Calculate();
-        System.out.println(calc.echo("aah"));
+	Calculate calc = new Calculate();
+    System.out.println(calc.echo("aah"));
     }
+
 }
