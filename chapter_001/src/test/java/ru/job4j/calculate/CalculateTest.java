4fa6d13aae57460a4523db64457b6245f01c1f62
Dmitriy Nikonorov <nikonorovds@yandex.ru>
2019-03-13 12:25:53 +0300

    add test

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+    @Test
+    public void whenEcho() {
+        Calculate calculate = new Calculate();
+        String hello = calculate.echo("Hello");
+        assertThat(hello, is("Hello Hello Hello"));
+    }
+
+}
