b2376f3efb15f99de2a8a8b042420b65d5b709d5
Dmitriy Nikonorov <nikonorovds@yandex.ru>
2019-04-17 15:16:35 +0300

    3.2. Конвертер валюты.

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -11,10 +11,7 @@ public class Converter {
      * @return Евро
      */
     public int rubleToEuro(int value){
-        int a1=1;
-        int b1=70*a1;
-        int c1 = b1;
-        return c1;
+        return value * 70;
     }
     /**
      *Конвертируем рублив доллары.
@@ -23,10 +20,7 @@ public class Converter {
      */
 
     public int rubleToDollar(int value){
-        int a2=1;
-        int b2=60*a2;
-        int c1 = b2;
-        return c1;
+        return value * 60;
     }
     /**
      *Конвертируем рублив доллары.
@@ -34,11 +28,7 @@ public class Converter {
      *@return рубли
      */
     public int euroToRuble(int value){
-        int a3=1;
-        int b3=70;
-        int c3 = b3/70*a3;
-        int c1 =c3;
-        return c1;
+        return value / 70;
     }
     /**
      *Конвертируем рублив доллары.
@@ -46,11 +36,7 @@ public class Converter {
      *@return рубли
      */
     public int dollarToRuble(int value){
-        int a4=1;
-        int b4=60;
-        int c4 = b4/60*a4;
-        int c1 = c4;
-        return c1;
+        return value / 60;
     }
 
 }
--- chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -8,27 +8,27 @@ public class ConverterTest {
     @Test
     public void when60RubleToDollarThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToDollar(60);
+        int result = converter.rubleToDollar(1);
         assertThat(result, is(60));
     }
 
     @Test
     public void when70RubleToEuroThen1() {
         Converter aconverter = new Converter();
-        int result = aconverter.rubleToEuro(70);
+        int result = aconverter.rubleToEuro(1);
         assertThat(result, is(70));
     }
 
     @Test
     public void when1EuroToRubleThen70() {
         Converter bconverter = new Converter();
-        int result = bconverter.euroToRuble(1);
+        int result = bconverter.euroToRuble(70);
         assertThat(result, is(1));
     }
     @Test
     public void when1DollarToRubleThen60() {
         Converter cconverter = new Converter();
-        int result = cconverter.dollarToRuble(1);
+        int result = cconverter.dollarToRuble(60);
         assertThat(result, is(1));
     }
 }
