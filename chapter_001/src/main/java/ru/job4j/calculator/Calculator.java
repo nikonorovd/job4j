b6b55b0dcc8077e1211f168e515cd258b2730ce7
Dmitriy Nikonorov <nikonorovds@yandex.ru>
2019-04-17 23:03:15 +0300

    3.4. Расстояние между точками в системе координат(javadoc на каждый метод)

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -10,19 +10,40 @@ public class Calculator {
      * Method to add some double
      * @param  first param
      * @param  second param
-     * @return result of add, subtract, div, multiple
+     * @return result of add
      */
 
     public double add(double first, double second) {
         return first + second;
 
     }
+    /**
+     * Method to subtract some double
+     * @param  first param
+     * @param  second param
+     * @return result of subtract
+     */
     public double subtract (double first, double second){
         return first - second;
     }
+
+    /**
+     * Method to div some double
+     * @param  first param
+     * @param  second param
+     * @return result of div
+     */
     public double div (double first, double second){
+
         return first / second;
     }
+
+    /**
+     * Method to multiple some double
+     * @param  first param
+     * @param  second param
+     * @return result of multiple
+     */
     public double multiple (double first, double second)
     {
         return first * second;
