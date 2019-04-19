5fdd10606767e0d0505b8b2c976aea00c5c7ff49
Dmitriy Nikonorov <nikonorovds@yandex.ru>
2019-04-19 14:58:51 +0300

    4.1. Глупый бот.

---
+++ chapter_001/src/main/java/ru/job4j/condition/TruckerJob.java
@@ -0,0 +1,44 @@
+package ru.job4j.condition;
+//
+//import java.sql.Driver;
+//public class TruckerJob {
+//    public static void main(String[]args){
+//        Driver steve = new Driver();
+//        System.out.println("Steve wants the trucker job");
+//        if (!steve.hasLisence()){
+//            System.out.println("Steve need to get a license!");
+//            steve.passExamOn("A");
+//        }
+//
+//        if (steve.canDrive("C")){
+//            System.out.println("Steve works on truscers!");
+//            }
+//        else if(steve.canDrive ("B")){
+//            System.out.println("Steve works in taxi!");
+//        }else {
+//            System.out.println("Steve does not have a job ");
+//        }
+//
+//    }
+//}
+
+
+//Продемонстрировать применение конструкции if-else-if
+//class IfElse {
+//    public static void main(String args[]) {
+//        int month = 4; // Апрель
+//        String season;
+//        if (month == 12 || month == 1 || month == 2)
+//            season = " зиме ";
+//        else if (month == 3 || month == 4 || month == 5)
+//            season = "весне";
+//        else if (month == 6 || month == 7 || month == 8)
+//            season = "лету 11 ";
+//        else if (month == 9 || month == 10 || month == 11)
+//            season = "осени 1";
+//        else
+//            season = "вымышленным месяцам";
+//        System.out.println("Апрель относится к " + season + ".");
+//    }
+//}
+
