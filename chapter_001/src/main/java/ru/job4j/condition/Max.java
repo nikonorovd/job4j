b2184723409adbd520d53da83a47801beaae738b
Dmitriy Nikonorov <nikonorovds@yandex.ru>
2019-04-20 13:47:30 +0300

    4.2. Максимум из двух чисел.

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,16 @@
+package  ru.job4j.condition;
+
+public class Max{
+    public int max (int left, int right){
+        int result = left;
+        return result;
+    }
+
+    public int maxChislo(){
+    int first = 10;
+    int second = 15;
+    int result1 = first+second;
+    return result1;
+    }
+
+}
