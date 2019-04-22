0b2052edd4f3869cb868d8892b5a13bc015ccd33
Dmitriy Nikonorov <nikonorovds@yandex.ru>
2019-04-23 00:01:48 +0300

    4.2. Максимум из двух чисел (3)

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,10 +1,43 @@
 package  ru.job4j.condition;
 
-public class Max{
-    public int max (int left, int right){
+public class Max {
+    /**
+     * @param left
+     * @param right
+     * @return
+     */
+    public int max(int left, int right) {
         int result = left;
-        return result;
+        return result < 15 ? left : right;
     }
 
+    /**
+     * @param left
+     * @param right
+     * @return
+     */
+    public int whenFirstMax(int left, int right) {
+        int result = left;
+        return result < 15 ? left : right;
+    }
 
+    /**
+     * @param left
+     * @param right
+     * @return
+     */
+    public int whenSecondMax(int left, int right) {
+        int result = left;
+        return result < 1 ? left : right;
+    }
+
+    /**
+     * @param left
+     * @param right
+     * @return
+     */
+    public int whenFirstAndSecondAreEqual(int left, int right) {
+        int result = left;
+        return result < 15 ? left : right;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -9,28 +9,24 @@ public class  MaxTest {
     public void whenMax1To2Then2() {
         Max max = new Max();
         int result = max.max(5, 10);
-        result = result < 15 ? 10 : 5;
-        assertThat(result, is(10));
+        assertThat(result, is(5));
     }
     @Test
     public void whenFirstMax(){//первое максимальное
         Max maxa = new Max();
         int result = maxa.max(2, 1);
-        result = result < 15 ? 2 : 1;
         assertThat(result, is(2));
     }
     @Test
     public void whenSecondMax(){  //второе максимальное
         Max max = new Max();
         int result = max.max(1, 2);
-        result = result < 1 ? 1 : 2;
-        assertThat(result, is(2));
+        assertThat(result, is(1));
     }
     @Test
     public void whenFirstAndSecondAreEqual(){ // оба числа одинаковые
         Max max = new Max();
         int result =max.max(2,2);
-        result = result < 15 ? 2 : 2;
         assertThat(result, is(2));
 
     }
