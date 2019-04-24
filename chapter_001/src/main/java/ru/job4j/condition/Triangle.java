ad17ee5fbe47313ce4e5f8c063e157c8defbe846
Dmitriy Nikonorov <nikonorovds@yandex.ru>
2019-04-24 22:56:51 +0300

    4.3. Вычисление площади треугольника (вопрос)

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -12,42 +12,44 @@ public class Triangle {
      * @param a расстояние между точками b c
      * @return Перимент
      */
-    public double period(double a, double b, double c){
-        return -1;
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
     }
+
     /**
      * Метод должен вычислить прощадь треугольканива.
-     *
+     * <p>
      * Формула.
-     *
+     * <p>
      * √ p *(p - ab) * (p - ac) * (p - bc)
-     *
+     * <p>
      * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
      *
      * @return Вернуть прощадь, если треугольник существует или -1.
      */
-    public double area (int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
         double rsl = -1;
         double a = new Point().distance(x1, y1, x2, y2);
         double b = new Point().distance(x2, y2, x3, y3);
         double c = new Point().distance(x1, y1, x3, y3);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
-            rsl = -1;
+            Math.sqrt(p * (p - a * b) * (p - a * c) * (p - b * c));
         }
         return rsl;
     }
+
     /**
      * Метод проверяет можно ли построить треугольник с такими длинами сторон.
-     *
+     * <p>
      * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
      *
-     * @param a Длина от точки a b.
-     * @param b Длина от точки a c.
-     * @param c Длина от точки b c.
+     * @param ab Длина от точки a до точки b.
+     * @param ac Длина от точки a до точки c.
+     * @param bc Длина от точки b до точки c.
      * @return
      */
-    private boolean exist(double a, double c, double b){
+    private boolean exist(double ab, double ac, double bc) {
         return false;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea(){
+        //Создаем объект "Треугольник"
+        Triangle triangle = new Triangle();
+        //вычисляем площадь
+        double result = triangle.area(0,0,0,2,0,0);
+        //Задаем ожидаемый результат
+        double expected = 2D;
+        //Проверяем результат и ожидаемое значение
+        assertThat (result, closeTo(expected, 0.1));
+    }
+}
